1cee5c33391cd773dc24502982a7e8cc4eeb14ab
andrepopoff <andrepopoff@gmail.com>
2018-12-01 18:34:32 +0400

    Added get_data() function. Wrote comments

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ from bs4 import BeautifulSoup
 
 
 def get_html(url):
+    """Returns the HTML code of the page."""
     response = requests.get(url)
     return response.text
 
 
+def get_data(html):
+    """Returns the text in the <h1> tag for the wordpress.org"""
+    soup = BeautifulSoup(html, 'lxml')
+    h1 = soup.find('div', id='home-welcome').find('header').find('h1').text
+    return h1
+
+
 def main():
     url = 'https://wordpress.org'
-    print(get_html(url))
+    print(get_data(get_html(url)))
 
 
 if __name__ == '__main__':
