d81f295c1862660ad8edb895401d21bf315dc15b
andrepopoff <andrepopoff@gmail.com>
2018-12-16 18:46:21 +0400

    Finished small parts, the script is completely ready

--- youtube-parser.py
+++ youtube-parser.py
@@ -1,3 +1,7 @@
+"""
+This script parses the name of the video and links to the video from the youtube channel and writes data in csv file
+"""
+
 import requests
 import sys
 import csv
@@ -5,9 +9,22 @@ import csv
 from bs4 import BeautifulSoup
 
 
-def get_html(url):
+BASE_URL = 'https://www.youtube.com'
+
+
+def get_response(url):
     response = requests.get(url)
     return response
+
+
+def get_html(response, json_header):
+    if 'html' in response.headers['Content-Type']:
+        return response.text
+    elif 'json' in response.headers['Content-Type']:
+        return response.json()[json_header]
+    else:
+        print('Unknown server response type')
+        sys.exit(0)
 
 
 def write_csv(data):
@@ -17,34 +34,23 @@ def write_csv(data):
 
 
 def get_page_data(response):
-    if 'html' in response.headers['Content-Type']:
-        html = response.text
-    elif 'json' in response.headers['Content-Type']:
-        html = response.json()['content_html']
-    else:
-        print('Unknown server response type')
-        sys.exit(0)
-
+    html = get_html(response, 'content_html')
     soup = BeautifulSoup(html, 'lxml')
     items = soup.find_all('h3', class_='yt-lockup-title')
 
     for item in items:
         name = item.text.strip()
         url = item.find('a').get('href')
-        data = {'name': name, 'url': 'https://www.youtube.com' + url}
+        data = {'name': name, 'url': BASE_URL + url}
         write_csv(data)
 
 
 def get_next_url(response):
-    if 'html' in response.headers['Content-Type']:
-        html = response.text
-    else:
-        html = response.json()['load_more_widget_html']
-
+    html = get_html(response, 'load_more_widget_html')
     soup = BeautifulSoup(html, 'lxml')
 
     try:
-        url = 'https://www.youtube.com' + soup.find('button', class_='load-more-button').get('data-uix-load-more-href')
+        url = BASE_URL + soup.find('button', class_='load-more-button').get('data-uix-load-more-href')
     except:
         url = ''
 
@@ -52,15 +58,10 @@ def get_next_url(response):
 
 
 def main():
-    # url = 'https://www.youtube.com/browse_ajax?ctoken=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk5yZ0JBQSUzRCUzRA%253D%253D&continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk5yZ0JBQSUzRCUzRA%253D%253D&itct=CBwQybcCIhMI8o37o9aV3wIVw5RVCh1E9AsI'
-    # url = 'https://www.youtube.com/browse_ajax?ctoken=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk5iZ0JBQSUzRCUzRA%253D%253D&continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk5iZ0JBQSUzRCUzRA%253D%253D&itct=CBwQybcCIhMIobnTodaV3wIVg7xVCh3MOQNp'
+    url = BASE_URL + '/user/schafer5/videos?gl=RU'
 
-    url = 'https://www.youtube.com/user/schafer5/videos?gl=RU'
-    # url1 = 'https://www.youtube.com/browse_ajax?action_continuation=1&amp;continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk1yZ0JBQSUzRCUzRA%253D%253D;&amp;direct_render=1'
-    # url2 = 'https://www.youtube.com/browse_ajax?action_continuation=1&amp;continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk03Z0JBQSUzRCUzRA%253D%253D'
-    # get_page_data(get_html(url))
     while url:
-        response = get_html(url)
+        response = get_response(url)
         get_page_data(response)
         url = get_next_url(response)
 
