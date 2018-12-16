539da0ba8a654e316cf6b0a5602dc380f191ea4c
andrepopoff <andrepopoff@gmail.com>
2018-12-16 22:07:59 +0400

    Added get_proxy function

--- proxy-parser.py
+++ proxy-parser.py
@@ -3,16 +3,28 @@ from bs4 import BeautifulSoup
 
 
 def get_html(url):
+    # proxies = {'https': 'ipaddress:5000'}
     response = requests.get(url)
     return response.text
 
 
 def get_proxy(html):
     soup = BeautifulSoup(html, 'lxml')
-    pass
+    trs = soup.find('table', id='proxylisttable').find_all('tr')[1:11]
+    proxies = []
+
+    for tr in trs:
+        tds = tr.find_all('td')
+        ip = tds[0].text.strip()
+        port = tds[1].text.strip()
+        schema = 'https' if 'yes' in tds[6].text.strip() else 'http'
+        proxy = {'schema': schema, 'address': ip + ':' + port}
+        proxies.append(proxy)
+
 
 def main():
-    pass
+    url = 'https://free-proxy-list.net/'
+    get_proxy(get_html(url))
 
 
 if __name__ == '__main__':
