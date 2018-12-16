862a24ad4ecdacaa8b53ec58b9dcbca93f14dd84
andrepopoff <andrepopoff@gmail.com>
2018-12-16 18:00:39 +0400

    The script works. It remains to finish the small parts

--- youtube-parser.py
+++ youtube-parser.py
@@ -31,8 +31,24 @@ def get_page_data(response):
     for item in items:
         name = item.text.strip()
         url = item.find('a').get('href')
-        data = {'name': name, 'url': url}
+        data = {'name': name, 'url': 'https://www.youtube.com' + url}
         write_csv(data)
+
+
+def get_next_url(response):
+    if 'html' in response.headers['Content-Type']:
+        html = response.text
+    else:
+        html = response.json()['load_more_widget_html']
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    try:
+        url = 'https://www.youtube.com' + soup.find('button', class_='load-more-button').get('data-uix-load-more-href')
+    except:
+        url = ''
+
+    return url
 
 
 def main():
@@ -41,8 +57,12 @@ def main():
 
     url = 'https://www.youtube.com/user/schafer5/videos?gl=RU'
     # url1 = 'https://www.youtube.com/browse_ajax?action_continuation=1&amp;continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk1yZ0JBQSUzRCUzRA%253D%253D;&amp;direct_render=1'
-    url2 = 'https://www.youtube.com/browse_ajax?action_continuation=1&amp;continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk03Z0JBQSUzRCUzRA%253D%253D'
-    get_page_data(get_html(url))
+    # url2 = 'https://www.youtube.com/browse_ajax?action_continuation=1&amp;continuation=4qmFsgJAEhhVQ0NleklnQzk3UHZVdVI0X2diRlVzNWcaJEVnWjJhV1JsYjNNZ0FEZ0JZQUZxQUhvQk03Z0JBQSUzRCUzRA%253D%253D'
+    # get_page_data(get_html(url))
+    while url:
+        response = get_html(url)
+        get_page_data(response)
+        url = get_next_url(response)
 
 
 if __name__ == '__main__':
